ed9c6e8b420f7c18f9d9261cc4e2f81ddc3144cb
Daniil Firsov <daniil-firsov@mail.ru>
2021-12-14 15:02:02 +0300

    Added user photo item

--- feature/src/main/java/ru/spbstu/feature/event/presentation/EventFragment.kt
+++ feature/src/main/java/ru/spbstu/feature/event/presentation/EventFragment.kt
@@ -1,8 +1,13 @@
 package ru.spbstu.feature.event.presentation
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import coil.load
+import coil.transform.CircleCropTransformation
 import ru.spbstu.common.di.FeatureUtils
+import ru.spbstu.common.extenstions.setDebounceClickListener
 import ru.spbstu.common.extenstions.viewBinding
 import ru.spbstu.common.utils.ToolbarFragment
 import ru.spbstu.feature.R
@@ -26,7 +31,15 @@ class EventFragment : ToolbarFragment<EventViewModel>(
 
     override fun setupViews() {
         super.setupViews()
+        binding.frgEventLayoutToolbar.includeToolbarTvTitle.text = "Комната 228"
         binding.frgEventRvPurchases.adapter = purchaseAdapter
+        binding.frgEventLayoutUser.itemUserIv.load("https://images.pexels.com/photos/5493447/pexels-photo-5493447.jpeg?cs=srgb&dl=pexels-olia-danilevich-5493447.jpg&fm=jpg") {
+            transformations(CircleCropTransformation())
+        }
+        binding.frgEventLayoutUser.itemUserIbDelete.visibility = View.GONE
+        binding.frgEventLayoutUser.itemUserIbDelete.setDebounceClickListener {
+            Toast.makeText(requireContext(), "Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun subscribe() {
