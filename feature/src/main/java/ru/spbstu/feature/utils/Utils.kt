19d6722945bfe3cd4bbe3d1347236346a14656a7
Bublyaev Aleksey <leshasochi2014@gmail.com>
2022-03-07 14:55:59 +0300

    added tests for calculating debt

---
+++ feature/src/main/java/ru/spbstu/feature/utils/Utils.kt
@@ -0,0 +1,18 @@
+package ru.spbstu.feature.utils
+
+import ru.spbstu.feature.domain.model.Expense
+
+object Utils {
+
+    fun calculateTotalDebt(list: List<Expense>, yourParticipantId: Long): Double {
+        var total = 0.0
+        list.forEach { expense ->
+            if (expense.users.containsKey(yourParticipantId) &&
+                expense.users[yourParticipantId] == false &&
+                expense.buyer.id != yourParticipantId) {
+                total += expense.price / expense.users.size
+            }
+        }
+        return total
+    }
+}
